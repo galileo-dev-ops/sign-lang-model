dc8fcc3a59f89c0484b65bce6ea0e0caf064fb75
galileo-dev-ops <142727059+galileo-dev-ops@users.noreply.github.com>
2024-03-09 14:51:20 +0000

    -

--- data_collection.py
+++ data_collection.py
@@ -7,7 +7,7 @@ cap = cv2.VideoCapture(0)
 detector = HandDetector(maxHands=1)
 offset = 20
 imgSize = 300
-folder = "Data/C"
+folder = "Data/B"
 counter = 0
 while True:
     success, img = cap.read()
