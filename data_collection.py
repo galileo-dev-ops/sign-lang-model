0c16db5376041f09463f3ac3bb433541fb53588f
pmkha <pmkhambhaita@outlook.com>
2024-03-09 16:56:50 +0000

    -

--- data_collection.py
+++ data_collection.py
@@ -7,7 +7,7 @@ cap = cv2.VideoCapture(0)
 detector = HandDetector(maxHands=1)
 offset = 20
 imgSize = 300
-folder = "Data/A"
+folder = "Data/C"
 counter = 0
 while True:
     success, img = cap.read()
@@ -26,32 +26,28 @@ while True:
 
         if h > 0 and w > 0:
             imgCrop = img[y - offset:y + h + offset, x - offset:x + w + offset]
-            # Rest of your code...
-        else:
-            print("Cropped image is empty. Skipping this frame.")
-
-        imgCrop = img[y - offset:y + h + offset, x - offset:x + w + offset]
-        imgCropShape = imgCrop.shape
-        aspectRatio = h / w
-        if aspectRatio > 1:
-            k = imgSize / h
-            wCal = math.ceil(k * w)
-            imgResize = cv2.resize(imgCrop, (wCal, imgSize))
-            imgResizeShape = imgResize.shape
-            wGap = math.ceil((imgSize - wCal) / 2)
-            imgWhite[:, wGap:wCal + wGap] = imgResize
-        else:
-            k = imgSize / w
-            hCal = math.ceil(k * h)
-            imgResize = cv2.resize(imgCrop, (imgSize, hCal))
-            imgResizeShape = imgResize.shape
-            hGap = math.ceil((imgSize - hCal) / 2)
-            imgWhite[hGap:hCal + hGap, :] = imgResize
-        cv2.imshow("ImageCrop", imgCrop)
-        cv2.imshow("ImageWhite", imgWhite)
-    cv2.imshow("Image", img)
-    key = cv2.waitKey(1)
-    if key == ord("s"):
-        counter += 1
-        cv2.imwrite(f'{folder}/Image_{time.time()}.jpg',imgWhite)
-        print(counter)
+            if imgCrop.size > 0:
+                imgCropShape = imgCrop.shape
+                aspectRatio = h / w
+                if aspectRatio > 1:
+                    k = imgSize / h
+                    wCal = math.ceil(k * w)
+                    imgResize = cv2.resize(imgCrop, (wCal, imgSize))
+                    imgResizeShape = imgResize.shape
+                    wGap = math.ceil((imgSize - wCal) / 2)
+                    imgWhite[:, wGap:wCal + wGap] = imgResize
+                else:
+                    k = imgSize / w
+                    hCal = math.ceil(k * h)
+                    imgResize = cv2.resize(imgCrop, (imgSize, hCal))
+                    imgResizeShape = imgResize.shape
+                    hGap = math.ceil((imgSize - hCal) / 2)
+                    imgWhite[hGap:hCal + hGap, :] = imgResize
+                cv2.imshow("ImageCrop", imgCrop)
+                cv2.imshow("ImageWhite", imgWhite)
+            cv2.imshow("Image", img)
+            key = cv2.waitKey(1)
+            if key == ord("s"):
+                counter += 1
+                cv2.imwrite(f'{folder}/Image_{time.time()}.jpg',imgWhite)
+                print(counter)
